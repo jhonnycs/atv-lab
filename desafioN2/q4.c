0d5a2f31b5919047623a64c7b10ba582465c09c3
jhonnycs <jhonnycunha7@gmail.com>
2024-08-09 15:36:03 -0300

    corrigida questão 4

--- desafioN2/q4.c
+++ desafioN2/q4.c
@@ -7,13 +7,15 @@ int condition(int value);
 void printArray(int array[], int size);
 
 int main() {
-    int array[] = {1, -2, 3, 5, 6};
+    int array[] = {8, -2, 3, 5, 6};
 
     int (*pCondition)(int) = condition;
 
     int result = filter(array, pCondition, sizeof(array) / sizeof(array[0]));
 
     printf("Novo tamanho do array: %d\n", result);
+
+    printArray(array, result);
 
     return 0;
 }
@@ -27,9 +29,7 @@ int filter(int *array, int (*condition)(int), int size) {
     
     for (int i = 0; i < size; i++) {
         if (condition(array[i])) {
-            if (condition(array[i])) {
-                array[newSize++] = array[i];
-            }
+            array[newSize++] = array[i];
         }
     }
     return newSize;
